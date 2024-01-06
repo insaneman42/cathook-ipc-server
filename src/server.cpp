4889c15b4e8f56dcdde16aafec716637e71d2223
Insaneman <148940879+insaneman42@users.noreply.github.com>
2024-01-05 18:34:48 -0800

    Update server.cpp
    cancer

--- src/server.cpp
+++ src/server.cpp
@@ -60,21 +60,10 @@ void print_status() {
 	peer().pool->statistics(info);
 	printf("memory: ");
 	TEXT_NORMAL; printf("%.1f%% free\n", float(info.free) / float(info.free + info.alloc) * 100); TEXT_BOLD;
-	/*printf("command memory pool stats: ");
-	CatMemoryPool::pool_info_s info;
-	peer().pool->statistics(info);
-	ESC_CUP(4, 8); ESC_EL(2);  printf("total:     ");
-	ESC_CUP(4, 9); ESC_EL(2);  printf("free:      ");
-	ESC_CUP(4, 10); ESC_EL(2); printf("allocated: ");
-	TEXT_NORMAL;
-	ESC_CUP(16, 8);  printf("%lu (%u blocks)", info.free + info.alloc, info.blkcnt);
-	ESC_CUP(16, 9); printf("%lu (%u blocks)", info.free, info.freeblk);
-	ESC_CUP(16, 10); printf("%lu (%u blocks)", info.alloc, info.allocblk);
-	*/
 	int ypos = 8;
 	ESC_CUP(2, ypos);
 	printf("%-2s %-5s %-9s %-21s %s\n", "ID", "PID", "SteamID", "Server IP", "Name");
-	printf("    %-5s %-9s %-4s   %-5s   %-5s  %-9s %s", "State", "Class", "Team", "Score", "Total", "Health", "Heartbeat");
+	printf(" %-5s %-9s %-4s   %-5s   %-5s  %-9s %s", "State", "Class", "Team", "Score", "Total", "Health", "Heartbeat");
 	ypos += 3;
 	TEXT_NORMAL;
 	// Zeroth peer is the server.
@@ -84,7 +73,7 @@ void print_status() {
 			const auto& data = peer().memory->peer_user_data[i];
 			printf("%-2u %-5d %-9ld %-21s %s\n", i, peer().memory->peer_data[i].pid, data.friendid, data.ingame.server, data.name);
 			if (data.connected && data.ingame.good) {
-				printf("    %-5s %-9s %-4s   %-5d   %-5d   %-4d/%-4d %u\n",
+				printf(" %-5s %-9s %-4s   %-5d   %-5d   %-4d/%-4d %u\n",
 						data.ingame.life_state ? "Dead" : "Alive",
 						good_class(data.ingame.role) ? classes[data.ingame.role].c_str() : classes[0].c_str(),
 						good_team(data.ingame.team) ? teams[data.ingame.team].c_str() : teams[0].c_str(),
@@ -92,7 +81,7 @@ void print_status() {
 						time(nullptr) - data.heartbeat);
 			} else {
 				printf("    %-5s %-9s %-4s   %-5s   %-5d   %-9s %u\n",
-						"N/A", "N/A", "N/A", "N/A", data.accumulated.score, "N/A", time(nullptr) - data.heartbeat);
+					"N/A", "N/A", "N/A", "N/A", data.accumulated.score, "N/A", time(nullptr) - data.heartbeat);
 			}
 			ypos += 2;
 		}
@@ -114,7 +103,7 @@ int main(int argc, char** argv) {
 	printf("cathook IPC server\n");
 	while (true) {
 		tick++;
-		if (!(tick % 10)) { // Sweep/Process once every 10 seconds
+		if (!(tick % 30)) { // Sweep/Process once every 30 seconds
 			peer().SweepDead();
 			peer().ProcessCommands();
 		}
